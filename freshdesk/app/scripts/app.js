a6ad4d13838afb1a1aa275b187591b3cd54fd63b
Saif Ali Shaik <saifshine7@gmail.com>
2021-11-08 23:49:37 +0530

    📦 NEW: implement save, delete features in the app

--- freshdesk/app/scripts/app.js
+++ freshdesk/app/scripts/app.js
@@ -1,99 +1,80 @@
-/**
- * @desc - This app allows you to save a memo linked to the ticket
- * @info - https://developers.freshdesk.com/v2/docs/data-storage/
- */
+var noteKey, client;
 
-let noteKey;
+(async function init() {
+  client = await app.initialized();
+  let {
+    loggedInUser: { id: agentId }
+  } = await client.data.get('loggedInUser');
+  let {
+    ticket: { id: ticketId }
+  } = await client.data.get('ticket');
 
-(function () {
-  app.iniialized(_client).then(
-    () => {
-      window.client = _client;
-      client.events.on('app.activated', () => {
-        fetchInfo(() => {
-          displayNote();
-          pick('#note-save').addEventListener('click', () => {
-            savenote();
-          });
-          pick('#note-delete').addEventListener('click', () => {
-            deletenote();
-          });
-        });
-      });
-    },
-    (err) => {
-      showerror(err);
-    }
-  );
+  noteKey = `${agentId}:${ticketId}`;
 
-  function showerror(err) {
-    client.interface
-      .trigger('showNotify', {
-        type: 'warning',
-        title: 'Warning',
-        message: `Error: App is facing issues during initialzation - ${err}`
-      })
-      .then(function (data) {
-        console.info(`Err: Interface API - ${data}`);
-      })
-      .catch(function (error) {
-        console.error(`Some error Encountered: ${error}`);
-      });
-  }
+  pick('.note-save').addEventListener('click', savenotes);
+  pick('.note-delete').addEventListener('click', deletenotes);
 
-  function savenote() {
-    pick('#note-save').addEventListener('click', () => {
-      var noteData = pick('#note').value;
-      if (noteData == '') {
-        notify('warning', 'Note is empty');
-        return;
-      }
-      client.db.set(noteKey, { note: noteData }).then(
-        () => {
-          notify('success', 'Note has been stored');
-        },
-        () => {
-          notify('danger', 'Error storing the note');
-        }
-      );
-    });
-  }
-
-  function deletenote() {
-    client.db.delete(noteKey).then(
-      function () {
-        pick('#note').value = '';
-        notify('success', 'Note has been deleted');
-      },
-      function () {
-        notify('danger', 'Error deleting the note');
-      }
-    );
-  }
-
-  function displayNote() {
-    client.db.get(noteKey).then((data) => {
-      pick('#note').value = data.note;
-    });
-  }
-
-  function notify(status, message) {
-    client.interface.trigger('showNotify', {
-      type: status,
-      message: message
-    });
-  }
-
-  function fetchInfo(callback) {
-    client.data.get('loggedInUser').then((userData) => {
-      client.data.get('ticket').then((ticketData) => {
-        noteKey = `${userData.loggedInUser.id} : ${ticketData.ticket.id}`;
-        callback();
-      });
-    });
-  }
+  client.events.on('app.activated', setup);
 })();
+
+async function savenotes() {
+  let notes = pick('.note').value;
+  if (notes) {
+    await to(client.db.set(noteKey, { notes }));
+    showNoticeToUser('success', 'DB — ', 'Notes saved');
+  } else {
+    showNoticeToUser('warning', 'DB — ', 'You did not save any notes');
+  }
+}
+
+async function deletenotes() {
+  let [err] = await to(client.db.delete(noteKey));
+  if (err) {
+    showNoticeToUser('error', 'DB — ', 'Not able to Delete');
+  }
+
+  pick('.note').value = '';
+  showNoticeToUser('success', 'DB — ', 'Notes deleted');
+}
+
+async function setup() {
+  let [err, { notes }] = await to(client.db.get(noteKey));
+
+  if (err) {
+    switch (err.status) {
+      case 404:
+        await showNoticeToUser('info', 'DB — ', 'You did not save any notes');
+        break;
+      case 0:
+        console.error('In development mode');
+        break;
+      default:
+        await showNoticeToUser('danger', 'Unexpected Error', "Take Developer's help");
+        break;
+    }
+  }
+  pick('.note').value = notes;
+}
+
+async function showNoticeToUser(...info) {
+  let [type, title, message] = info;
+  let notificationType = { type, title, message };
+
+  await client.interface.trigger('showNotify', notificationType);
+}
 
 function pick(selector, context = document) {
   return context.querySelector(selector);
+}
+
+// utility to handle
+function to(promise, improved) {
+  return promise
+    .then((data) => [null, data])
+    .catch((err) => {
+      if (improved) {
+        Object.assign(err, improved);
+      }
+      return [err];
+    });
 }
